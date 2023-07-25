3fe212b23a9805e6c5a419c9be6b5d13494a3a3f
Mohammed Hassan Latif <106679087+MohammedHassanLatif93@users.noreply.github.com>
2023-07-25 18:45:21 +0100

    WIP fixing database issues

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-s62vxjusv3elt6qe-fsnd.uk.auth0.com', // the auth0 domain prefix
+    url: 'dev-s62vxjusv3elt6qe-fsnd.uk', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: 'dSUfDOnuhq96CEa2GiydHszvJ8LRSzYj', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'https://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
